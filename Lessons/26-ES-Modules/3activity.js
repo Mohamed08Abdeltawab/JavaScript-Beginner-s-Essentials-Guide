03a7805b7c6f393b5b2bde92852af59eb64636d0
Mohamed08Abdeltawab <mohamedzidan0999@gmail.com>
2026-08-22 01:52:43 +0300

    26 -> 3 activities

---
+++ Lessons/26-ES-Modules/3activity.js
@@ -0,0 +1,16 @@
+
+/*
+//1 -> true
+create  mofules for math.js, user.js and api.js
+
+//2
+math.js => export const PI;
+user.js => export defaulte Users{} --class
+api.js => export function getUserbyid(){}
+
+//3
+input validation 
+formatting helper
+andy function that help in other function 
+internal calculations
+*/
